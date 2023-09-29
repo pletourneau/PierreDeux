03b114254bddebe22c778c6ec788b608a50ef07a
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-29 09:36:23 -0700

    add vendor description to conform to client specs

--- PierreDeux/Models/Vendor.cs
+++ PierreDeux/Models/Vendor.cs
@@ -6,12 +6,14 @@ namespace PierreDeux.Models
   {
     private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
+    public string Description { get; set; }
     public int Id { get; }
     public List<Order> Orders { get; set; }
     
     public Vendor(string vendorName)
     {
       Name = vendorName;
+      Description = vendorDescription;
       _instances.Add(this);
       Id = _instances.Count;
       Orders = new List<Order>{};
