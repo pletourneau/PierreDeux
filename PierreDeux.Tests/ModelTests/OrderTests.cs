46505f150cbe5a6cace0a98ad90f9dbfc2ca3f25
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-29 11:07:31 -0700

    pass first order test

--- PierreDeux.Tests/ModelTests/OrderTests.cs
+++ PierreDeux.Tests/ModelTests/OrderTests.cs
@@ -3,7 +3,21 @@ using System.Collections.Generic;
 using PierreDeux.Models;
 using System;
 
-namespace Order.Tests
+namespace PierreDeux.Tests
 {
-  
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test", "title", 13.31f);
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
   }
+}
--- PierreDeux/Models/Order.cs
+++ PierreDeux/Models/Order.cs
@@ -1,9 +1,26 @@
 using System.Collections.Generic;
+using System;
 
 namespace PierreDeux.Models
 {
   public class Order
   {
-   
+   public string Description { get; set; }
+   public string Title { get; set; }
+   public int Id { get; }
+
+   public float Price { get; set; }
+   public DateTime Date { get; set; }
+   private static List<Order> _instances = new List<Order> {};
+
+   public Order(string Description, string title, float Price)
+   {
+    
+
+   }
+   public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
